7c8fbbe4fce58f9b8309d6001410a6c839c511cc
perspectsite <39679309+perspectsite@users.noreply.github.com>
2023-01-06 21:38:08 -0500

    Update index.js

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -3,7 +3,7 @@ const Footer = () => {
   return (
     <footer>
       <div className="container-fluid px-6 md:flex justify-between items-center text-center my-4">
-        <p className="font-semibold">© {currentYear} {perspect.site_title}</p>
+        <p className="font-semibold">© {currentYear} {window.perspect.site_title}</p>
         <a className="font-semibold" href="https://perspect.com">
           Powered by Perspect Ecommerce
         </a>
